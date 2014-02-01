ae49c316cf9a5b04c5ad592feca12af86d2b0dfd
Akihiro Yamamoto <fkmks247@gmail.com>
2014-02-01 12:13:32 +0900

    add phpfiles for Testing

---
+++ phpfiles/Foo.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is to use workshop, then this code is not mote.
+ * @author yamashiro
+ *
+ */
+class Foo {
+	function thisIsCopyAndPastePrograming($val) {
+		$val = $val + 1;
+		
+		if ($val === 1) {
+			return "1";
+		} else if ($val === 2) {
+			return "2";
+		} else if ($val === 3) {
+			return "3";
+		} else if ($val === 4) {
+			return "4";
+		} else if ($val === 5) {
+			return "5";
+		} else if ($val === 6) {
+			return "6";
+		} else if ($val === 7) {
+			return "7";
+		}
+		
+		return "Foo";
+	}
+	
+	function plus($a, $b) { 
+		return $a * $b;
+	}
+} 
---
+++ phpfiles/Hoge.php
@@ -0,0 +1,43 @@
+<?php
+class Hoge {
+  function thisIsCopyAndPastePrograming($val) {
+    if ($val === 1) {
+      return "1";
+    } else if ($val === 2) {
+      return "2";
+    } else if ($val === 3) {
+      return "3";
+    } else if ($val === 4) {
+      return "4";
+    } else if ($val === 5) {
+      return "5";
+    } else if ($val === 6) {
+      return "6";
+    } else if ($val === 7) {
+      return "7";
+    } 
+    return "Hoge";
+  }
+  function hasUnUsedParameterMethod($unuseParameter, $useParameter) {
+    return $useParameter;
+  }
+	
+  function complexMethod($array) {
+    foreach($array as $val) {
+      if ($val === 3) {
+	if (true) {
+	  if (true) {
+	    while(true) {
+	      if (true) {} 
+	      if (true) {}
+	      return $val;
+	    }
+	  }
+	}
+      }
+    }
+    if (true) { while (false) { if (true) {} } }
+    if (true) { while (false) { if (true) {} } }
+    return "complexMethod"; 
+  }
+}
